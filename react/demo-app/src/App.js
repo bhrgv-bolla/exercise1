7cf28ce025a766b65e8035aeaf6a3d9874d04695
Bhargav Bolla <hire.bhargav@gmail.com>
2018-09-03 20:00:40 -0700

    add react code

--- react/demo-app/src/App.js
+++ react/demo-app/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PhotoGallery from "./demo";
+
+const images = [
+    {
+        url: 'http://lorempixel.com/1000/600/nature/1/',
+        caption: "one pic",
+    },
+    {
+        url: 'http://lorempixel.com/1000/600/nature/3/',
+        caption: "two pic",
+    }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+          <PhotoGallery data={images}/>
     );
   }
 }
---
+++ react/demo-app/src/demo/index.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+
+const styles = {
+    outerDiv: {
+      height: 500,
+      width: 700,
+        margin: "pink 2px solid"
+    },
+    img: {
+        height: 500,
+        width: 700,
+        position: "absolute",
+    },
+    caption: {
+        position: "absolute",
+        bottom: "10%"
+    },
+    leftPress: {
+        position: "absolute",
+        width: "50%",
+        height: "100%",
+        backgroundColor: "orange",
+        opacity: 0.2,
+    },
+    rightPress: {
+        position: "absolute",
+        width: "50%",
+        left: "50%",
+        height: "100%",
+        backgroundColor: "green",
+        opacity: 0.2,
+    }
+};
+
+/**
+ * The PhotoGallery component should take in an array of images.
+ • Each image will have a url and a caption.
+ • The captions should be displayed over the image.
+ • The PhotoGallery should allow users to either click or swipe through the images.
+ • Bonus credit for smoothly animating the swipe and transition between images.
+ • Handle any edge cases you can think of.
+ */
+export default class PhotoGallery extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentIdx: 0
+        };
+        this.handleLeftPress = this.handleLeftPress.bind(this);
+        this.handleRightPress = this.handleRightPress.bind(this);
+    }
+
+    handleLeftPress() {
+        if(this.state.currentIdx === 0) return;
+        this.setState({
+            currentIdx: this.state.currentIdx - 1
+        });
+    }
+
+    handleRightPress() {
+        if(this.state.currentIdx === this.props.data.length - 1) return;
+        this.setState({
+            currentIdx: this.state.currentIdx + 1
+        });
+    }
+
+
+    render() {
+        const {data} = this.props;
+        const {currentIdx} = this.state;
+        return (
+          <div style={styles.outerDiv}>
+              <img src={data[currentIdx].url} style={styles.img}/>
+
+              <span style={styles.caption}>
+                  {data[currentIdx].caption}
+              </span>
+
+              <span style={styles.leftPress} onClick={this.handleLeftPress}></span>
+              <span style={styles.rightPress} onClick={this.handleRightPress}></span>
+          </div>
+        );
+    }
+
+
+
+}
